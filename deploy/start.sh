170742ddf6eb6466ebf550fd59f2bd9023352c58
davutozcan <davutozcan87@gmail.com>
2018-10-12 15:34:23 -0400

    standart output forwarded to file

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo "starting $(date)" >> /tmp/bookmarkreader.log
+echo "starting $(date)" >> /tmp/bookmarkreader.txt
 cd /home/davut/projects/BookmarkReader-Root/backend/
 echo $(pwd) >> /tmp/bookmarkreader.txt
-/home/davut/.nvm/versions/node/v9.5.0/bin/node bin/www >> /tmp/bookmarkreader.txt
+/home/davut/.nvm/versions/node/v9.5.0/bin/node bin/www >> /tmp/bookmarkreader.txt 2>&1
