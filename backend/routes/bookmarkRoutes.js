6238738f5600d62ec420032b6b71f00a3df693f4
davutozcan <davutozcan87@gmail.com>
2018-11-05 22:54:54 +0300

    path method of bookmark added

--- backend/routes/bookmarkRoutes.js
+++ backend/routes/bookmarkRoutes.js
@@ -12,8 +12,47 @@ router.get('/:googleId/bookmarks', function (req, res, next){
         console.log("found user" , user);
         res.status(200).send(user.bookmarks);
     }).catch(err=>{
-        res.status(404).send({ msg:`user not found with gmail id "${googleId}"` , error:err});
+        userNotFound(res, googleId, err);
     });
 });
 
+router.patch("/:googleId/bookmarks" , (req , resp , next)=>{
+    let received = req.body;
+    let oldValue = received.oldValue;
+    let newValue = received.newValue;
+    let googleId = req.params.googleId;
+    console.log(`replacing ${oldValue} -> ${newValue}`);
+    USERS.findByGoogleId(googleId)
+        .then(user=>{
+            if(!user)
+                userNotFound(resp , googleId , null);
+            else{
+                let index = user.bookmarks.indexOf(oldValue);
+                if(index == -1)
+                    throw `user does not contains: ${oldValue}`;
+                user.bookmarks[index] = newValue;
+                return USERS.update({googleId:googleId} , user);
+            }
+        })
+        .then(update=>{
+            console.log("updated ->" , update);
+            return USERS.findByGoogleId(googleId);
+        })
+        .then(user=>{
+            resp.status(200).send(user);
+        })
+        .catch(e=>{
+            internalError(resp ,e);
+        })
+});
+
 module.exports = router;
+
+function userNotFound(res, googleId, err) {
+    res.status(404).send({ msg: `user not found with google id "${googleId}"`, error: err });
+}
+
+function internalError(res , err)
+{
+    res.status(500).send({message:"request failed" , error : err});
+}
