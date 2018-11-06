696c9bd58c5011c0d57a801cd0a4d40c7ce6d7f7
davutozcan <davutozcan87@gmail.com>
2018-11-06 22:41:50 +0300

    post and delete working

--- backend/routes/bookmarkRoutes.js
+++ backend/routes/bookmarkRoutes.js
@@ -8,7 +8,8 @@ router.get('/:googleId/bookmarks', function (req, res, next){
 
     console.log("google id ->" , googleId);
     console.log("google id lenght ->" , googleId.length);
-    USERS.findByGoogleId(googleId).then(user =>{
+    findByGoogleId(googleId)
+    .then(user =>{
         console.log("found user" , user);
         res.status(200).send(user.bookmarks);
     }).catch(err=>{
@@ -22,17 +23,13 @@ router.patch("/:googleId/bookmarks" , (req , resp , next)=>{
     let newValue = received.newValue;
     let googleId = req.params.googleId;
     console.log(`replacing ${oldValue} -> ${newValue}`);
-    USERS.findByGoogleId(googleId)
+    findByGoogleId(googleId)
         .then(user=>{
-            if(!user)
-                userNotFound(resp , googleId , null);
-            else{
-                let index = user.bookmarks.indexOf(oldValue);
-                if(index == -1)
-                    throw `user does not contains: ${oldValue}`;
-                user.bookmarks[index] = newValue;
-                return USERS.update({googleId:googleId} , user);
-            }
+            let index = user.bookmarks.indexOf(oldValue);
+            if(index == -1)
+                throw new CustomException(400 , `Invalid request` , `User does not contains ${oldValue}`);
+            user.bookmarks[index] = newValue;
+            return USERS.update({googleId:googleId} , user);
         })
         .then(update=>{
             console.log("updated ->" , update);
@@ -42,41 +39,87 @@ router.patch("/:googleId/bookmarks" , (req , resp , next)=>{
             resp.status(200).send(user);
         })
         .catch(e=>{
-            internalError(resp ,e);
+            errorResponse(resp ,e);
         })
 });
 
 router.delete("/:googleId/bookmarks" , (req , resp , next)=>{
     let bookmarksToDelete = req.body;
     let googleId = req.params.googleId;
-    USERS.findByGoogleId(googleId)
-        .then(user=>{
-            if(!user)
-                throw `User "${googleId}" not found`;
-            return user;
-        }).then(user=>{
+    findByGoogleId(googleId)
+    .then(user=>{
             user.bookmarks = user.bookmarks.filter(item => !bookmarksToDelete.includes(item));
             return user;
-        }).then(user =>{
-              return USERS.update({googleId} , user);
-        }).then(update =>{
+        })
+    .then(user =>{
+              update(user);
+        })
+    .then(update =>{
             return USERS.findByGoogleId(googleId);
-        }).then(user=>{
+        })
+    .then(user=>{
             resp.status(200).send(user);
-        }).catch(e=>{
-            internalError(resp , e);
+        })
+    .catch(e=>{
+            errorResponse(resp , e);
         });
 });
 
+router.post("/:googleId/bookmarks" , (req , resp , next)=>{
+    let googleId = req.params.googleId;
+    let bookmarkArray = req.body;
+    findByGoogleId(googleId)
+        .then(user=>{
+            if(!Array.isArray(bookmarkArray) || !isStringArray(bookmarkArray))
+                throw new CustomException(400 , `Not a string array -> ${bookmarkArray}` , "request body must be string array");
+            user.bookmarks = user.bookmarks.concat(bookmarkArray.filter(item => !user.bookmarks.includes(item)) );
+            return update(user);
+        }).then(()=>{
+            return findByGoogleId(googleId);
+        }).then(user=>{
+            resp.status(200).send(user);
+        }).catch(e=>{
+            errorResponse(resp , e);
+        });
+});
 
+function findByGoogleId(googleId)
+{
+    return USERS.findByGoogleId(googleId)
+    .then(user=>{
+        if(!user)
+            throw new CustomException(404 , `User "${googleId}" not found` , "Not found");
+        return user;
+    });
+}
+
+function update(user){
+    return USERS.update({googleId:user.googleId} , user);
+}
+
+class CustomException{
+    constructor(status , message , error){
+        this.status = status;
+        this.message = message;
+        this.error = error;
+    }
+}
 
 function userNotFound(res, googleId, err) {
     res.status(404).send({ msg: `user not found with google id "${googleId}"`, error: err });
 }
 
-function internalError(res , err)
+function errorResponse(res , err)
 {
-    res.status(500).send({message:"request failed" , error : err});
+    if(err instanceof CustomException)
+    {
+        res.status(err.status).send({message:err.message ,error:err.error });
+    }else
+        res.status(500).send({message:"request failed" , error : err});
+}
+
+function isStringArray(arr){
+    return arr.find(item => typeof item !== 'string') === undefined;
 }
 
 module.exports = router;
