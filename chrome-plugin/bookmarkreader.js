bcd5dc0ea4071b5531312d5dec8dd6115525dc4d
davutozcan <davutozcan87@gmail.com>
2018-11-21 00:37:37 +0300

    bookmarks displayed on tab

--- chrome-plugin/bookmarkreader.js
+++ chrome-plugin/bookmarkreader.js
@@ -1,10 +1,36 @@
 
 
-function dumpBookmarks(){
-  $("#bookmarks").append("<p>loaded..</p>");
+function dumpBookmarks() {
+  var bookmarkTreeNodes = chrome.bookmarks.getTree(
+    function (bookmarkTreeNodes) {
+      $('#bookmarks').append(dumpTreeNodes(bookmarkTreeNodes));
+    });
 }
 
+function dumpTreeNodes(bookmarkNodes) {
+  var list = $('<ul>');
+  var i;
+  for (i = 0; i < bookmarkNodes.length; i++) {
+    list.append(dumpNode(bookmarkNodes[i]));
+  }
+  return list;
+}
 
+function dumpNode(bookmarkNode) {
+   console.log("node ->" , bookmarkNode);
+   if(!bookmarkNode.children)
+   {
+     return $('<li class="single_url">').append(bookmarkNode.url);
+   }else{
+     let root = $('<li>').append(bookmarkNode.title);
+     bookmarkNode.children.forEach(element => {
+       let subRoot= $('<ul>');
+       subRoot.append(dumpNode(element));
+       root.append(subRoot);
+     });
+     return root;
+   }
+}
 
 document.addEventListener('DOMContentLoaded', function () {
   dumpBookmarks();
